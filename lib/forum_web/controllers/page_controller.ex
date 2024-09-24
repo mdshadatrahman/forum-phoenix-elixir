d6f3c15e59648c0f455352222677499ad61dd505
Shadat Rahman <shadat.rahman.464@gmail.com>
2024-09-25 02:55:35 +0600

    created migration

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -2,8 +2,15 @@ defmodule ForumWeb.PageController do
   use ForumWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     render(conn, :home, layout: false)
+  end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Shabab", email: "shabab@email.com"},
+      %{id: 2, name: "Sharika", email: "sharika@email.com"},
+      %{id: 3, name: "Shadat", email: "shadat@email.com"},
+    ]
+    json(conn, %{users: users})
   end
 end
--- lib/forum_web/router.ex
+++ lib/forum_web/router.ex
@@ -16,14 +16,15 @@ defmodule ForumWeb.Router do
 
   scope "/", ForumWeb do
     pipe_through :browser
-
     get "/", PageController, :home
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", ForumWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", ForumWeb do
+    pipe_through :api
+    resources "/users", UserController, except: [:new, :edit]
+    resources "/posts", PostController, except: [:new, :edit]
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:forum, :dev_routes) do
